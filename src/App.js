70c2bf1684fc5414903aba1407004a1929f4b25f
delanobmarques <delanobmarques@gmail.com>
2022-08-15 14:32:48 -0300

    initial react project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
+  const handleNameChange = () => {
+    const names = ['Bob', ' Kevin' , 'Dave', 'James', 'Anna', 'Mary', 'Nicolle'];
+    const int = Math.floor(Math.random() * 7);
+    return names[int];
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <p>
+        Hello {handleNameChange()}!
+      </p>
+      
     </div>
   );
 }
+
 
 export default App;
