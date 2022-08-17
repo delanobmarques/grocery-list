eb632030bee8a2a189ba97e0f5ae366ef503388c
delanobmarques <delanobmarques@gmail.com>
2022-08-17 10:28:34 -0300

    add handle-check and handle-delete methods

--- src/components/content.component.jsx
+++ src/components/content.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
 
@@ -20,22 +21,49 @@ const Content = () => {
         },
     ]);
 
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
   return (
     <main>
-        <ul>
+        { items.length ? (
+            <ul>
             {
                 items.map((item) => (
                     <li className="item" key={item.id}>
                         <input
                             type="checkbox"
-                            checked={item.checked}
+                            onChange={ () => { handleCheck(item.id)} }
+                            checked={ item.checked }                            
                         />
-                        <label>{item.item}</label>
-                        <button>Delete</button>
+                        <label 
+                            style={ (item.checked) ? { textDecoration: 'line-through'} : null }
+                            onDoubleClick={ () => { handleCheck(item.id)} }
+                        >
+                            {item.item}
+                        </label>
+                        <FaTrashAlt 
+                            onClick={() => handleDelete(item.id)}
+                            role="button" 
+                            tabIndex="0" 
+                        />
                     </li>
                 ))
             }
-        </ul>
+            </ul>
+        ) : (
+            <p style={{color:"#aaf", marginTop:"2rem"}}>Your Shopping List is Empty</p>
+        )}
+        
     </main>
   )
 }
