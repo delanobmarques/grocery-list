9fb7da9467d77c89cf3b849a59a514ce777aaed1
delanobmarques <delanobmarques@gmail.com>
2022-08-22 11:24:15 -0300

    add add-item component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import { useState } from "react";
+import AddItem from "./components/add-item";
 import Content from "./components/content.component";
 import Footer from "./components/footer.component";
 import Header from "./components/header.component";
 
 function App() {  
 
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: false,
-        item: "Eggs",
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Apples",
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Milk",
-    },
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const [ newItem, setNewItem ] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length-1].id + 1 : 1;
+    const myNewItem = { id, checked:false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
   }
 
   return(
     <div className="App">
       <Header title="Shopping List"/>     
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content 
         items={ items }
         handleCheck = { handleCheck }
